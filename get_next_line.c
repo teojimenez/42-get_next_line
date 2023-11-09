e0d45a2c181640d996238edeef20d2c4b86ca8f4
teojimenez <teojimbru@gmail.com>
2023-11-09 15:14:32 +0100

    update 09/11 ~ Everything works, but LEAKS

--- get_next_line.c
+++ get_next_line.c
@@ -52,10 +52,10 @@ char	*ft_strjoin(char *s1, char *s2)
 	char	* result;
 	if (!s1)
 	{
-		s1 = (char *)malloc(1);
+		s1 = (char *)malloc(1); //new
 		if(!s1)
 		{
-			free(s1);
+			// free(s1); //new
 			return NULL;
 		}
 		s1[0] = '\0';
@@ -63,13 +63,17 @@ char	*ft_strjoin(char *s1, char *s2)
 	str = (char *)malloc((ft_strlen(s1) + ft_strlen(s2) + 1)  * sizeof(char));
     // str[ft_strlen(s1) + ft_strlen(s2) + 1] = '\0';
 	if (!str)
+	{
+		free(s1); //new
 		return (0);
+	}
 	result = str;
 	while (*s1)
 		*str++ = *s1++;
 	while (*s2)
 		*str++ = *s2++;
     *str = '\0';
+	// free(s1); //new
 	return (result);
 }
 
@@ -83,10 +87,9 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		return (0);
 	// if (start > (unsigned int)ft_strlen(s))
 	// 	return (ft_strdup(""));
-	else if (len > ft_strlen(s) - start)
+	if (len > ft_strlen(s) - start)
 		len = ft_strlen(s) - start;
 	sub = malloc(len + 1 * sizeof(char));
-	sub[len + 1] = '\0';
 	if(!sub)
 	{
 		free(sub);
@@ -98,17 +101,19 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	return ((char *)sub);
 }
 
-void xtraFunction(char **storage, char *buffer, char **result)
+void xtraFunction(char **storage, char **result)
 {
 		int i = 0;
 		//tamaño de result, desde storage
 		while ((*storage)[i] != '\n')
 			i++;
 		i++;
+		//free(*result);
+		*result = 0;
 		*(result) = (char *)malloc((i + 1) * sizeof(char));
 		if(!(*result))
 		{
-			free(*result);
+			// free(*result); //new
 			return ;
 		}
 		int j = 0;
@@ -134,12 +139,16 @@ void xtraFunction(char **storage, char *buffer, char **result)
 		//storage debe ser storage pero sin lo de buffer
 		// free(*storage);
 		// (*storage) = 0;
-		int buff_len = ft_strlen(buffer);
+		// int buff_len = ft_strlen(buffer);
 
 		//caso en que hay mas cosas despues del buffer
 		// if (buff_len > i)
 		int len = ft_strlen(*(storage)) - i;
-		*(storage) = ft_substr(*(storage), i, len);
+		// free(*storage); //new
+		if (len > 0)
+			*(storage) = ft_substr(*(storage), i, len);
+		else
+			*(storage) = "";
 }
 
 char	*ft_strdup(const char *s1)
@@ -160,76 +169,128 @@ char	*ft_strdup(const char *s1)
 	return (memory);
 }
 
-char *ft_read(char *storage)
-{
-	/*
-	CREO UN BUFFER Y LEO LA CANTIDAD QUE ME DAN DEL BUFFER SIZE
-	N VECES HASTA QUE TENGA UN BARRA N COMO MINIMO EN EL BUFFER
-	READ()
-	*/
-}
+// void ft_read_storage(char **storage, char *buffer, char **result)
+// {
+// 	while(!ft_strchr(buffer, '\n') && bytes_read > 0) 
+// 	{
+        
+// 		bytes_read = read(fd, buffer, BUFFER_SIZE);
+// 		// -------------- Rellenar/Actualizar store -------------
+// 		buffer[bytes_read] = '\0';
+// 		storage = ft_strjoin(storage, buffer);
+//         // -------------------------------------------
+// 		if (bytes_read == 0)
+// 		{
+// 			result = ft_strdup(storage);
+// 			break;
+// 			//free(*storage);
+// 			//*storage = NULL;
+// 		}
+// 		else if(ft_strchr(storage, '\n')) //
+// 			xtraFunction(&storage, buffer, &result);
+// 	}
+// }
 
-/*
-	hola como 
-	estas
-	soy
-*/
+
+// char *ft_read_real(char *storage)
+// {
+// 	/*
+// 	CREO UN BUFFER Y LEO LA CANTIDAD QUE ME DAN DEL BUFFER SIZE
+// 	N VECES HASTA QUE TENGA UN BARRA N COMO MINIMO EN EL BUFFER
+// 	READ()
+// 	*/
+// }
 
 char *get_next_line(int fd)
 {
     static char *storage = NULL;
-    char *buffer;
-	char *result;
+    char *buffer = NULL; //paco said
+	char *result = NULL;
 	size_t bytes_read = 0;
+
+		//siempre lees del storage pero no nunca lo llenas
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	if (!storage || (storage && !ft_strchr(storage, '\n'))) //caso storage primera iteracion, y storage tiene contenido pero no es barra \n
-		storage = ft_read();//read()
-	if (!storage)
-		return (NULL);
-	//siempre lees del storage pero no nunca lo llenas
-	//funcion  
-	// ex
+	if (!storage || (storage && !ft_strchr(storage, '\n'))) //caso storage primera iteracion, y storage tiene contenido pero no es barra \n, y ultimo donde bytes read es 0
+	{
+		buffer = (char*)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+		if (!buffer)
+		{
+			free(storage);
+			return (NULL);
+		}
+		buffer[0] = '\0';
+		bytes_read = 1;
+		while(!storage || ((storage && !ft_strchr(storage, '\n')) && bytes_read > 0))
+		{
+			
+			bytes_read = read(fd, buffer, BUFFER_SIZE);
+			// -------------- Rellenar/Actualizar store -------------
+			buffer[bytes_read] = '\0';
 
-    buffer = (char*)malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	if (!buffer)
-	{
-		free(storage);
-		return (NULL);
-	}
-	
-	bytes_read = read(fd, buffer, BUFFER_SIZE);
-	if (bytes_read <= 0)
-	{
-		free(storage);
-		return NULL;
-	}
-	buffer[bytes_read] = '\0';
-	storage = ft_strjoin(storage, buffer);
-	if (ft_strchr(storage, '\n'))
-		xtraFunction(&storage, buffer, &result);
-
-	//cuando queremos leer?????
-	while(!ft_strchr(buffer, '\n') && bytes_read > 0) 
-	{
-        
-		bytes_read = read(fd, buffer, BUFFER_SIZE);
-		// -------------- Rellenar/Actualizar store -------------
-		buffer[bytes_read] = '\0';
-		storage = ft_strjoin(storage, buffer);
-        // -------------------------------------------
+			storage = ft_strjoin(storage, buffer);
+			// -------------------------------------------
+			if(ft_strchr(storage, '\n')) //
+			{
+				xtraFunction(&storage, &result);
+				break;
+			}
+		}
 		if (bytes_read == 0)
 		{
 			result = ft_strdup(storage);
-			break;
+			// break;
 			//free(*storage);
 			//*storage = NULL;
 		}
-		else if(ft_strchr(storage, '\n')) //
-			xtraFunction(&storage, buffer, &result);
+		// free(buffer);
+		return result;
+	}// ft_read_real();//read()
+		
+	if (!storage)
+		return (NULL);
+	if (storage && ft_strchr(storage, '\n')) //caso hay \n en storage y hay que leer storage y devolver
+	{
+		xtraFunction(&storage, &result);
+		// free(buffer);
+		return result;
 	}
-    free(buffer);
+	
+	// bytes_read = read(fd, buffer, BUFFER_SIZE);
+	// if (bytes_read <= 0)
+	// {
+	// 	free(storage);
+	// 	return NULL;
+	// }
+	// buffer[bytes_read] = '\0';
+	// storage = ft_strjoin(storage, buffer);
+	// if (ft_strchr(storage, '\n'))
+	// 	xtraFunction(&storage, buffer, &result);
+
+	// //cuando queremos leer?????
+	// while(!ft_strchr(buffer, '\n') && bytes_read > 0) 
+	// {
+        
+	// 	bytes_read = read(fd, buffer, BUFFER_SIZE);
+	// 	// -------------- Rellenar/Actualizar store -------------
+	// 	buffer[bytes_read] = '\0';
+	// 	storage = ft_strjoin(storage, buffer);
+    //     // -------------------------------------------
+	// 	if (bytes_read == 0)
+	// 	{
+	// 		result = ft_strdup(storage);
+	// 		break;
+	// 		//free(*storage);
+	// 		//*storage = NULL;
+	// 	}
+	// 	else if(ft_strchr(storage, '\n')) //
+	// 		xtraFunction(&storage, buffer, &result);
+	// }
+
+    // free(storage);
+	free(buffer); //new
+	// free(result);
 	return (result);
 }
 
@@ -312,12 +373,12 @@ char *get_next_line(int fd)
 
 int main()
 {
-    int fd = open("file.txt", O_RDONLY);
+    int fd = open("tests/file3.txt", O_RDONLY);
     if (fd < 0)
         return (0);
 	char	*str;
 	int i = 0;
-	while(i < 50)
+	while(i < 1)
 	{
 		str = get_next_line(fd);
         if (!str)
--- get_next_line.h
+++ get_next_line.h
@@ -17,7 +17,7 @@
 # include <unistd.h>
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 300
+#  define BUFFER_SIZE 100000
 # endif
 
 
